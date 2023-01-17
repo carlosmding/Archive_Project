cdad7d8160181d344c9ab8fd8397c63ce7487e5f
Carlos Alfredo Pinto Hernández <70851771+carlosmding@users.noreply.github.com>
2023-01-17 02:05:40 +0000

    Implementado el método de graficar torta para porcentaje de población mundial por pais

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from archive import file
-from utils import functions
+from utils import functions, charts
 
 data= file.extracting_data("./archive/data.csv")
-country = input("Ingres el pais a buscar=> ")
+
+"""country = input("Type the country => ").title()
 result = functions.find_country(country,data)
+try: 
+  labels, values = functions.find_labels_values(result[0])
+except IndexError:
+  print("We can´t find the country, please try again")
+charts.generate_bar_chart(labels, values)
+"""
+labels_pie, values_pie = functions.create_data_percentage(data)
+charts.generate_pie_chart(labels_pie, values_pie)
+
 
 
     
--- utils/functions.py
+++ utils/functions.py
@@ -3,4 +3,23 @@ def find_country(country_to_find, data):
   return result
 
 def find_labels_values(country):
-  
+  print(country)
+  result = {
+    '2022' : int(country['2022 Population']),
+    '2020' : int(country['2020 Population']),
+    '2015' : int(country['2015 Population']),
+    '2010' : int(country['2010 Population']),
+    '2000' : int(country['2000 Population']),
+    '1990' : int(country['1990 Population']),
+    '1980' : int(country['1980 Population']),
+    '1970' : int(country['1970 Population']),
+  }
+  return result.keys(), result.values()
+
+def create_data_percentage(data):
+  countries=[]
+  percentage =[]
+  for country in data:
+    countries.append(country["Country/Territory"])
+    percentage.append(country["World Population Percentage"])
+  return countries, percentage
